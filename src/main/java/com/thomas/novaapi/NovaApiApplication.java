9d704fa0d919baf3b0bf1947e8ce660b761084ed
RMTh0mas <renanmiguelthomas@gmail.com>
2023-01-29 21:19:21 -0300

    Versao inicial da nova api

---
+++ src/main/java/com/thomas/novaapi/NovaApiApplication.java
@@ -0,0 +1,13 @@
+package com.thomas.novaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thomas/novaapi/NovaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomas.novaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
