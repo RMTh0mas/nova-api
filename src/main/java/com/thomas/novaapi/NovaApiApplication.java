75bd0a9b24a1c58fe92b405f692f089a9dec4a68
RMTh0mas <renanmiguelthomas@gmail.com>
2023-01-29 21:29:36 -0300

    alteração no arquivo principal

--- src/main/java/com/thomas/novaapi/NovaApiApplication.java
+++ src/main/java/com/thomas/novaapi/NovaApiApplication.java
@@ -8,6 +8,7 @@ public class NovaApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NovaApiApplication.class, args);
+		System.out.println("Nova api...");
 	}
 
 }
