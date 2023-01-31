437dc5911278df66d06fd2881acb41762cc4df19
RMTh0mas <renanmiguelthomas@gmail.com>
2023-01-31 14:06:41 -0300

    alteração das dependências do maven e do pom.xml

---
+++ src/main/java/com/renan/minha_api_restful/MinhaApiRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.renan.minha_api_restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaApiRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaApiRestfulApplication.class, args);
+	}
+
+}
